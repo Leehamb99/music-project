fdf3b62d2e2b7fa1c2718fa98aad6db28b499343
Leehamb99 <leehamb99@gmail.com>
2022-09-27 21:11:16 +0100

    add songlist card

--- src/components/Music/Songs.js
+++
@@ -1,6 +0,0 @@
-import React, { useState } from 'react';
-import Artist from '../Artists/Artists';
-
-const Songs = () => {
-    if 
-}
---
+++ src/components/SongSection/SongSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SongList = (props) => {
+    return(
+        <>
+            <p> {props.song} </p>
+        </>
+    )
+}
+
+export default SongList;
